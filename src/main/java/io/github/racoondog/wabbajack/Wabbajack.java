483ef982ba04d26ae6bc40bec3df55f17ec99fd7
RacoonDog <32882447+racoondog@users.noreply.github.com>
2025-08-02 20:08:31 -0400

    project setup

---
+++ src/main/java/io/github/racoondog/wabbajack/Wabbajack.java
@@ -0,0 +1,15 @@
+package io.github.racoondog.wabbajack;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Wabbajack implements ModInitializer {
+	public static final String MOD_ID = "wabbajack";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}

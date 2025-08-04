2bb0d87dbe9f761e6569aaa108f051a887799eb9
RacoonDog <32882447+racoondog@users.noreply.github.com>
2025-08-04 04:49:37 -0400

    replace The Wabbajack!'s placeholder aoe effect with entity transmogrification

---
+++ src/main/java/io/github/racoondog/wabbajack/DataTags.java
@@ -0,0 +1,10 @@
+package io.github.racoondog.wabbajack;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+
+public final class DataTags {
+    public static final TagKey<EntityType<?>> CAN_TRANSFORM_INTO = TagKey.of(RegistryKeys.ENTITY_TYPE, Identifier.of(Wabbajack.MOD_ID, "can_transform_into"));
+}
--- src/main/java/io/github/racoondog/wabbajack/WabbajackProjectileEntity.java
+++ src/main/java/io/github/racoondog/wabbajack/WabbajackProjectileEntity.java
@@ -1,19 +1,23 @@
 package io.github.racoondog.wabbajack;
 
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.FlyingItemEntity;
-import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.*;
 import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.entity.projectile.ProjectileUtil;
 import net.minecraft.item.ItemStack;
 import net.minecraft.particle.DustParticleEffect;
+import net.minecraft.registry.Registries;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Colors;
+import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.RaycastContext;
 import net.minecraft.world.World;
+import net.minecraft.world.explosion.ExplosionImpl;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 public class WabbajackProjectileEntity extends ProjectileEntity implements FlyingItemEntity {
     private static final DustParticleEffect TRAIL_EFFECT = new DustParticleEffect(Colors.RED, 1f);
@@ -70,20 +74,40 @@ public class WabbajackProjectileEntity extends ProjectileEntity implements Flyin
         super.onCollision(hitResult);
 
         if (this.getWorld() instanceof ServerWorld serverWorld) {
-            serverWorld.createExplosion(
-                this,
-                this.getDamageSources().indirectMagic(this, this.getOwner()),
-                null,
-                this.getX(),
-                this.getY(),
-                this.getZ(),
-                3,
-                false,
-                World.ExplosionSourceType.MOB
-            );
+            Entity hit = null;
+            if (hitResult instanceof EntityHitResult entityHitResult) {
+                hit = entityHitResult.getEntity();
+            }
+
+            for (Entity entity : serverWorld.getOtherEntities(this, this.getBoundingBox().expand(3f))) {
+                if (!(entity instanceof LivingEntity)
+                    || entity instanceof PlayerEntity
+                    || entity.isInvulnerable()) continue;
+
+                if (entity == hit || (entity.squaredDistanceTo(this) < 3f * 3f && ExplosionImpl.calculateReceivedDamage(this.getPos(), entity) > 0.1f)) {
+                    entity.discard();
+
+                    getRandomEntity(entity.getType()).spawn(serverWorld, null, entity.getBlockPos(), SpawnReason.MOB_SUMMONED, false, false);
+                }
+            }
 
             this.discard();
         }
+    }
+
+    private static EntityType<? extends Entity> getRandomEntity(EntityType<?> current) {
+        EntityType<?> entityType = null;
+
+        int depth = 0;
+        while (entityType == null || !entityType.isIn(DataTags.CAN_TRANSFORM_INTO) || entityType == current) {
+            if (depth++ == 10) {
+                return EntityType.TADPOLE;
+            }
+
+            entityType = Registries.ENTITY_TYPE.get(ThreadLocalRandom.current().nextInt(Registries.ENTITY_TYPE.size()));
+        }
+
+        return entityType;
     }
 
     @Override

133607ea7f237401dc8e98bac253f3dda5c9de08
RacoonDog <32882447+racoondog@users.noreply.github.com>
2025-08-09 00:32:44 -0400

    use quadratic function for thunderbolt

--- src/main/java/io/github/racoondog/wabbajack/effects/types/ThunderboltEffect.java
+++ src/main/java/io/github/racoondog/wabbajack/effects/types/ThunderboltEffect.java
@@ -1,5 +1,6 @@
 package io.github.racoondog.wabbajack.effects.types;
 
+import io.github.racoondog.wabbajack.Wabbajack;
 import io.github.racoondog.wabbajack.WabbajackProjectileEntity;
 import io.github.racoondog.wabbajack.effects.WabbajackEffect;
 import net.minecraft.entity.EntityType;
@@ -8,11 +9,24 @@ import net.minecraft.entity.SpawnReason;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import org.jetbrains.annotations.Nullable;
 
 public class ThunderboltEffect extends WabbajackEffect {
     @Override
     public void onProjectileCollision(ServerWorld world, WabbajackProjectileEntity projectile, HitResult collision, @Nullable LivingEntity caster) {
+        float aoe = Wabbajack.CONFIG.aoeSize;
+        int strikes = MathHelper.ceil(0.2f * aoe * aoe + 1f);
+
         world.spawnEntity(EntityType.LIGHTNING_BOLT.spawn(world, BlockPos.ofFloored(collision.getPos()), SpawnReason.TRIGGERED));
+
+        if (strikes > 1) {
+            for (int i = 1; i < strikes; i++) {
+                double x = world.random.nextGaussian() * aoe / 2;
+                double z = world.random.nextGaussian() * aoe / 2;
+
+                world.spawnEntity(EntityType.LIGHTNING_BOLT.spawn(world, BlockPos.ofFloored(collision.getPos().add(x, 0, z)), SpawnReason.TRIGGERED));
+            }
+        }
     }
 }
